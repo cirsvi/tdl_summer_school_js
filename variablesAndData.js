e4e6ac0e84cb3fb0c8dfbc0b1ed5de2b0cb12960
cirsvi <s23cirsvija@venta.lv>
2026-07-15 09:47:25 +0300

    Adding first file with variables and data.

---
+++ variablesAndData.js
@@ -0,0 +1,18 @@
+// Double slash for a single line comment!
+
+/*
+Multiline comments in JS
+are made using slash and multiply!
+*/
+
+// 1. Print "Hello world!" to console:
+console.log("Hello World!");
+console.log('Hello World!');
+console.log(`Hello World <${1 + 2}`);
+
+// 2. Variables:
+// Using const keyword means that the value 
+// should not / can not be changed:
+const text = 'Hello World!';
+console.log(text);
+
